092a5485f01c3be9bf6c2d8949aee50cb002bb00
Anmol Rajbhandari <anmol.rajbhandari90@gmail.com>
2016-02-22 10:02:09 +0545

    Update on the look.

--- themes/missnepal/js/script.js
+++ themes/missnepal/js/script.js
@@ -1,5 +1,8 @@
 (function($){
     $(document).ready(function() {
+        //Set body height to be the same height as window
+        $('#region-content').css('height',$(window).height());
+        
        $(".view-featured-images .item-list ul").masonry({
 			itemSelector: ".view-featured-images .item-list ul li",
 			columnWidth: 220,
